b59e51796124bb0b1eafe5ef73fe72784d8ef741
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-06-06 23:22:01 +0200

    Add draw 16 divs with js and styling with css

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const appEl = document.querySelector('#app');
+
+for (let i = 0; i < 16; i++) {
+  const div = document.createElement('div');
+  div.className = 'grid-item';
+  appEl.appendChild(div);
+}
